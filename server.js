9c3669e68d417b460c26f5085f98ae33b1dd4051
prateekbose20011 <smitraprateek2001@gmail.com>
2021-02-28 12:57:12 +0530

    Added server.js File

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const firebase = require('firebase')
+const cors = require('cors')
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+var PORT = app.listen(process.env.PORT || 3000)
+app.listen(PORT)
+
+const firebaseConfig = YOUR_FIREBASE_CONFIG
+
+firebase.initializeApp(firebaseConfig)
+
+let database = firebase.database()
+
+app.get("/", (req, res) => {
+    res.send("New Server running")
+})
+
+app.post("/food", (req, res) => {
+    console.log(req.body.Phone)
+    database.ref(`/food/${req.body.Phone}`).set(req.body, (err) => {
+        if(err){
+            console.log('Error in writing')
+        } else {
+            console.log('Data added')
+        }
+    })
+})
+
+app.post("/waste", (req, res) => {
+    console.log(req.body.Phone)
+    database.ref(`/waste/${req.body.Phone}`).set(req.body, (err) => {
+        if(err){
+            console.log('Error in writing')
+        } else {
+            console.log('Data added')
+        }
+    })
+})
